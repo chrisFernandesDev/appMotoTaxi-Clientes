613a298e410d5da8e15bc691a34f2f93a22a2326
Christina Fernandes <christina.o.fernandes21@gmail.com>
2021-08-05 14:04:22 -0300

    Primeiro

---
+++ firebase.js
@@ -0,0 +1,13 @@
+import firebase from "firebase";
+import 'firebase/firestore';
+
+var firebaseConfig = {
+    apiKey: "AIzaSyAsGlc6KQz_20zWdo5YXhPpkw_x958lyEw",
+    authDomain: "motoapp-da5f9.firebaseapp.com",
+    projectId: "motoapp-da5f9",
+    storageBucket: "motoapp-da5f9.appspot.com",
+    messagingSenderId: "95280987880",
+    appId: "1:95280987880:web:5b3852376651e58cea135d"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
